2661a81a8f27202c3a6b654af4d54b0d2b169c7e
danii <daniil82484@gmail.com>
2023-02-10 20:56:34 +0500

    3

--- app/src/main/java/com/example/uilover/LoginActivity.kt
+++ app/src/main/java/com/example/uilover/LoginActivity.kt
@@ -5,13 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 
-    fun SignUp(view: View) {
+    fun login(view: View) {
         val intent = Intent(this,LoginActivity::class.java)
         startActivity(intent)
     }
